3c60a9ceac5e6a5972150d62e2025e4c79a5506f
Eugeny Okulik <tiburon854@gmail.com>
2023-12-27 13:26:42 +0300

    Files for lesson 8

---
+++ homework/eugene_okulik/Lesson_8/lesson_7.py
@@ -0,0 +1,30 @@
+# import random
+from random import random, randint, randrange, choice
+# from random import *
+from homework.eugene_okulik.utils.helper import assist as assist1
+from homework.eugene_okulik.utils.help2 import assist as assist2
+from homework.eugene_okulik.utils.help2 import very_useful_function_for_your_easy_life as useful
+
+# print()
+# input()
+# int()
+
+# print(random.random())
+# print(f'Your price is {int(random.random() * 100)}')
+# print(random.randint(0, 1))
+# print(random.randrange(0, 10, 2))
+# users = ['user11', 'user12', 'user100']
+# print(random.choice(users))
+
+print(random())
+print(f'Your price is {int(random() * 100)}')
+print(randint(0, 1))
+print(randrange(0, 10, 2))
+users = ['user11', 'user12', 'user100']
+print(choice(users))
+
+assist1()
+# print(helper.variable)
+while 'sldkjfl' == 'sdsdfsdf':
+    if 'sldkjfl' == 'sdsdfsdf':
+        print(f'skadflkasdjfhalsdkjhlaksdfjhlaksjdfh {useful()}')
